92e38737f53d25ef866246942d65e7ae24fb9c23
clhZhou <249117598@qq.com>
2020-10-21 23:19:52 +0800

    init

--- cache/src/main/java/com/luis/cache/CacheApplication.java
+++ cache/src/main/java/com/luis/cache/CacheApplication.java
@@ -1,16 +1,13 @@
-package com.luis.auth;
+package com.luis.cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author luis
- */
 @SpringBootApplication
-public class AuthApplication {
+public class CacheApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthApplication.class, args);
+        SpringApplication.run(CacheApplication.class, args);
     }
 
 }
