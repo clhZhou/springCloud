8cb029bac4ba3e3055f49063a17de112a39d549d
clhZhou <249117598@qq.com>
2020-11-09 22:35:12 +0800

    io init

---
+++ io/src/main/java/com/luis/io/IoApplication.java
@@ -0,0 +1,16 @@
+package com.luis.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luis
+ */
+@SpringBootApplication
+public class IoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoApplication.class, args);
+    }
+
+}
