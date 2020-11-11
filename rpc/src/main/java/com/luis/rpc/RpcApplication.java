359c733649fd55e0b81d1699f891b6ae45f73626
clhZhou <249117598@qq.com>
2020-11-11 23:43:11 +0800

    rpc init

---
+++ rpc/src/main/java/com/luis/rpc/RpcApplication.java
@@ -0,0 +1,16 @@
+package com.luis.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luis
+ */
+@SpringBootApplication
+public class RpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcApplication.class, args);
+    }
+
+}
