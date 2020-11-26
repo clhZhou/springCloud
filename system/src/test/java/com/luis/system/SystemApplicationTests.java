1c8d46f0c601b9baa0492f284c4bc0d79b474d2c
clhZhou <249117598@qq.com>
2020-11-26 23:32:19 +0800

    two database demo

--- system/src/test/java/com/luis/system/SystemApplicationTests.java
+++ system/src/test/java/com/luis/system/SystemApplicationTests.java
@@ -1,13 +1,23 @@
 package com.luis.system;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class SystemApplicationTests {
 
+    @Autowired
+    JdbcTemplate db1JdbcTemplate;
+    @Autowired
+    JdbcTemplate db2JdbcTemplate;
+
     @Test
-    void contextLoads() {
+    public void dbTest(){
+        String sql="insert into user_info(name,age) values('mic1',18)";
+        db1JdbcTemplate.execute(sql);
+        db2JdbcTemplate.execute(sql);
     }
 
 }
