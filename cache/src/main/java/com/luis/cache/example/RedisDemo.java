2843564e83589e816b602719b2622eaf66606e2b
clhZhou <249117598@qq.com>
2020-11-26 21:13:54 +0800

    redisTemplate demo

---
+++ cache/src/main/java/com/luis/cache/example/RedisDemo.java
@@ -0,0 +1,18 @@
+package com.luis.cache.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author luis
+ * @date 2020/11/25
+ */
+public class RedisDemo {
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    public void redis_(){
+//        redisTemplate.
+    }
+}
--- cache/src/test/java/com/luis/cache/CacheApplicationTests.java
+++ cache/src/test/java/com/luis/cache/CacheApplicationTests.java
@@ -1,13 +1,36 @@
 package com.luis.cache;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.UUID;
 
 @SpringBootTest
 class CacheApplicationTests {
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setStringSerializer(stringRedisSerializer);
+        redisTemplate.setDefaultSerializer(stringRedisSerializer);
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(stringRedisSerializer);
+//        redisTemplate.setEnableDefaultSerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(stringRedisSerializer);
+        System.out.println(redisTemplate.getStringSerializer());
+
+//        redisTemplate.opsForValue().set("name","周瑞");
+        System.out.println(redisTemplate.opsForValue().get("name"));
+        redisTemplate.opsForValue().set("primaryKey", UUID.randomUUID().toString());
+
     }
 
 }
