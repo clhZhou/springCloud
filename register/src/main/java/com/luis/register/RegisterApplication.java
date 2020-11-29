d736a1ba5ddc53189f858d7e09f8c036f7924449
clhZhou <249117598@qq.com>
2020-11-29 10:16:59 +0800

    register init

---
+++ register/src/main/java/com/luis/register/RegisterApplication.java
@@ -0,0 +1,16 @@
+package com.luis.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luis
+ */
+@SpringBootApplication
+public class RegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterApplication.class, args);
+    }
+
+}
