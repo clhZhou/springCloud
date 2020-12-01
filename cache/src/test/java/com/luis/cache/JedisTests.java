01250c43584587fa95af4798ce6d789d16edf765
clhZhou <249117598@qq.com>
2020-12-01 23:38:38 +0800

    Jedis use db

--- cache/src/test/java/com/luis/cache/JedisTests.java
+++ cache/src/test/java/com/luis/cache/JedisTests.java
@@ -14,19 +14,25 @@ public class JedisTests {
 
     @Test
     void contextLoads() {
-//        JedisDbUtil jedisDbUtil = new JedisDbUtil();
-//        jedisDbUtil.set("test1","123",1,false,null);
-//        jedisDbUtil.get("test", 1).toString();
-
         Jedis jedis = new Jedis("192.168.0.200",6379);
-        int num = 15;
-        jedis.select(num);
-//        jedis.set("key"+num,"0");
-        jedis.incr("key15");
-
-        if(jedis.get("key"+num).equals("5")){
-             jedis.set("key15","0");
+//        int num = 15;
+//        jedis.select(num);
+////        jedis.set("key"+num,"0");
+//        jedis.incr("key15");
+//
+//        if(jedis.get("key"+num).equals("5")){
+//             jedis.set("key15","0");
+//        }
+//        System.out.println(jedis.get("key"+num));
+        boolean keyExist = jedis.exists("nxKey");
+        System.out.println(keyExist);
+        long ttl1 = jedis.ttl("nxKey");
+        System.out.println(ttl1);
+        if(keyExist){
+            jedis.del("nxKey");
         }
-        System.out.println(jedis.get("key"+num));
+        long ttl = 100;
+        jedis.set("nxKey","nxValue","NX","EX",100);
+
     }
 }
